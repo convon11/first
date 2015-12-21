72aad81cde7732259d0a35504a6e5de8b3953cb5
convon <coreynordloh@nordloh.com>
2015-12-21 21:16:17 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,20 @@
 print("hello world")
+*originall test^
 u=5
+*u defined^
 if u==5:
    print("it worked")
 else:
   print("it didnt work")
+*u test^
 def rode(s):
    print("input name")
    print("hello,my name is "s)
+*rode command^
 age=input("enter your age:")
+*age test^
 if age=13:
    print("you are corey")
 else:
    print("you are not corey")
-   
+*rest of age test^
