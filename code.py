5bcf94982de44876262583ecdb235beb81037957
convon <coreynordloh@nordloh.com>
2015-12-21 21:10:58 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,15 @@
 print("hello world")
 u=5
-if u==5
+if u==5:
    print("it worked")
-else
+else:
   print("it didnt work")
 def rode(s):
    print("input name")
    print("hello,my name is "s)
+age=input("enter your age:")
+if age=13:
+   print("you are corey")
+else:
+   print("you are not corey")
+   
