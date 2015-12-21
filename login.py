249adda71e1a62adc6dcecae3366b5c46e7d196a
convon <coreynordloh@nordloh.com>
2015-12-21 23:02:43 +0500

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,11 @@
 username= input("your username is:")
+username_test= input("your username is:")
+password=input("your password is:")
+password_test=input("your password is:")
+if username==username_test:
+  print("correct")
+elif password==password_test:
+  print("correct")
+else:
+  print("incorrect")
+  
